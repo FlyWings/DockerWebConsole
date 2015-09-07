02b946900a9e2f2536bcab3b2c4384c232c7a01d
robfrut135 <robfrut@yahoo.es>
2015-09-07 18:58:49 +0200

    init basic front-end with template by W3layouts

--- src/runtime/run.go
+++ src/runtime/run.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-
+	"time"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -17,7 +17,7 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
-
+	"github.com/skratchdot/open-golang/open"
 	"os/exec"
 	"os"
 )
@@ -63,9 +63,9 @@ var (
 	templates_path = resources_path + "templates/"
 	text_path = resources_path +  "text/"
 
-	templates = template.Must(template.ParseFiles(templates_path + "/view.html", templates_path + "/home.html"))
+	templates = template.Must(template.ParseFiles("../resources/web/view.html", "../resources/web/index.html", "../resources/web/contact.html"))
 
-	validPath = regexp.MustCompile("^((/view/[a-zA-Z0-9]+)|(/home/))$")
+	validPath = regexp.MustCompile("^/(view|contact)/([a-zA-Z0-9]+)$")
 )
 
 type Page struct {
@@ -89,23 +89,56 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
 	c := make(chan bool)
 	go func() {
 		id := r.FormValue("id")
 		cmd := "gotty --once -w -p 9999 docker exec -ti " + id + " bash"
-
 		out, err := ssh.Run(cmd)
 		if err != nil {
-			panic("Can't run remote command: " + err.Error() + out + cmd)
+			panic("Can't run remote command: " + err.Error() + out)
 		}
-
 		c <- true
 	}()
+
+	time.Sleep(2 * time.Second)
+
+	open.RunWith("http://"+ host +":9999", "firefox")
+
 	// wait for the blocking function to finish if it hasn't already
 	<-c
 
-	http.Redirect(w, r, "http://"+ host +":9999", 301)
+	renderTemplate(w, "view", p)
+}
+
+func contactHandlerNil(w http.ResponseWriter, r *http.Request) {
+
+	renderTemplate(w, "contact", &Page{})
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request, title string) {
+	if title == "send" {
+		/*
+			TODO: send data
+		*/
+
+		http.Redirect(w, r, "/", 301)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+
+func renderTemplateNil(w http.ResponseWriter, tmpl string) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
@@ -142,13 +175,7 @@ func printOutput(outs []byte) {
   }
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request, title string) {
-
-	p, err := loadPage("home")
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 	containers := make(map[string]Container)
 
@@ -191,18 +218,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request, title string) {
 				log.Fatal(err)
 			}
 
-			containers[m.Id] = m
+			containers[m.Id[0:10]] = m
 		}
 	}
-	p.Containers = containers
 
-	renderTemplate(w, "home", p)
+	p := &Page{Containers: containers}
+
+	renderTemplate(w, "index", p)
 }
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/home/", makeHandler(homeHandler))
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandlerNil)
+
 	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/contact/", makeHandler(contactHandler))
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("../resources/web"))))
 
 	if *addr {
 		l, err := net.Listen("tcp", "127.0.0.1:0")
